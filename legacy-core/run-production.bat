e723ec5696614245b8c478e83b504c815a6f9ceb
User <user@example.com>
2025-05-21 06:05:35 -0500

    fix: delete production scripts

--- legacy-core/run-production.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Next.js standalone server in production mode...
-
-:: Copy static files to the correct location (only needed once, but we'll do it every time to be safe)
-xcopy /E /I /Y .next\static .next\standalone\.next\static
-
-:: Start the server
-cd .next\standalone
-node server.js 
